da146412e142de8fcc14d7f6dd39bd6bdaac2484
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-02-10 12:39:24 +0100

    register user

---
+++ src/js/api/auth/register.mjs
@@ -0,0 +1,20 @@
+import {API_SOCIAL_URL} from "../constants";
+
+const action = "/auth/register";
+const method = "post";
+
+export async function register(profile){
+const registerURL = API_SOCIAL_URL + action;
+const body = JSON.stringify(profile); 
+
+const respons = await fetch(registerURL, {
+    headers:{
+        "content-Type": "application/json"
+    },
+    method,
+    body
+})
+
+const result = await Response.json()
+console.log(result)
+}
---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,18 @@
+import { register } from "../api/auth/register.mjs";
+
+export function setRegisterFormListener(){
+    const form = document.querySelector("#loginForm");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const form = event.target;
+        
+        const formData = new FormData(form); 
+        const profile = Object.fromEntries(formData.entries())
+        const action = form.action; 
+        const method = form.method;
+
+        //Send it to the API 
+        register(profile, action, method)
+    }) 
+}
