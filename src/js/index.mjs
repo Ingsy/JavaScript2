31e3151582a293d8bf7218650fe6afeac379ca84
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-02-14 11:09:54 +0100

    post/edit

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,13 +1,19 @@
-import { setRegisterFormListener } from "./handlers/register.mjs";
-import { setLoginFormListener } from "./handlers/login.mjs";
-import * as post from "./api/posts/index.mjs";
+
+import * as listener from "./handlers/index.mjs";
+import * as templates from "./templates/index.mjs"
+import * as post from "./api/posts/index.mjs"
+
 
 const path = location.pathname; 
 
-if (path === "login/"){
-    setLoginFormListener()
-} else if (path === "register/"){
-    setRegisterFormListener()
+if (path === "login/") {
+    listeners.setLoginFormListener()
+} else if (path === "register/") {
+    listeners.setRegisterFormListener()
+} else if (path === "/post/create/") {
+    listeners.setCreatePostFormListeners()
+} else if (path === "/post/edit/") {
+    listeners.setUpdatePostListener()
 }
 
 // post.createPost()
@@ -16,4 +22,12 @@ if (path === "login/"){
 // post.getPost()
 // post.getPosts()
 
-post.getPost(640).then(console.log)
+// post.getPost(640).then(console.log)
+
+//async function testTemplate(){
+//    const posts = await postMethods.getPosts();
+//    const container = document.querySelector("#");
+//    templates.renderPostTemplates(posts, container);
+//}
+
+//testTemplate()
