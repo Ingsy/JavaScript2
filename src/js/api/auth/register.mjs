96a5f192d0eb26a05899d5144b73a24537b10b87
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-03-28 15:41:58 +0200

    added try/catch error/ and message for register

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -6,21 +6,33 @@ const action = "/auth/register";
 const method = "post";
 
 export async function register(profile) {
-    const registerURL = API_SOCIAL_URL + action;
-    const body = JSON.stringify(profile);
+    try {
+        const registerURL = API_SOCIAL_URL + action;
+        const body = JSON.stringify(profile);
 
-    const response = await fetch(registerURL, {
-        headers: {
-            "content-Type": "application/json"
-        },
-        method,
-        body
-    })
+        const response = await fetch(registerURL, {
+            headers: {
+                "content-Type": "application/json"
+            },
+            method,
+            body
+        })
 
-    const result = await response.json();
-    alert("You are now registered")
-    window.location.href = "/login.html"
-    return result
+        const result = await response.json();
+        if (!response.ok) {
+            alert("We could not register this account. Please try again")
+        } else {
+            alert("You are now registered. Please log in to precede");
+            setTimeout(function () {
+                window.location.href = "/login.html";
+            }, 2500);
+        }
+
+        return result;
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-
